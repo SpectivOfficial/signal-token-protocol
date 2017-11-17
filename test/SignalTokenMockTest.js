cad9032da147a0f79a1d1f2bfe3ca7d1fad364ab
Parker McCurley <parker@decentcrypto.com>
2017-11-17 15:23:31 -0500

    Complete test coverage for 'SignalTokenMock' smart contract

--- test/SignalTokenMockTest.js
+++ test/SignalTokenMockTest.js
@@ -61,4 +61,54 @@ contract("SignalTokenMock", function(balances) {
         );
       });
   });
+
+
+  it("should revert a transfer if sender's balance disallows", function() {
+    var signal_token_mock;
+
+    var account_one = balances[0];
+    var account_one_starting_balance;
+    var account_one_ending_balance;
+
+    var account_two = balances[1];
+    var account_two_starting_balance;
+    var account_two_ending_balance;
+
+    var amount = 1000000;
+
+    return SignalTokenMock.deployed()
+      .then(function(instance) {
+        signal_token_mock = instance;
+        return signal_token_mock.getBalance(account_one);
+      })
+      .then(function(balance) {
+        account_one_starting_balance = balance.toNumber();
+        return signal_token_mock.getBalance(account_two);
+      })
+      .then(function(balance) {
+        account_two_starting_balance = balance.toNumber();
+        return signal_token_mock.transfer(amount, account_two, {from: account_one});
+      })
+      .then(function() {
+        return signal_token_mock.getBalance(account_one);
+      })
+      .then(function(balance) {
+        account_one_ending_balance = balance.toNumber();
+        return signal_token_mock.getBalance(account_two);
+      })
+      .then(function(balance) {
+        account_two_ending_balance = balance.toNumber();
+
+        assert.equal(
+          account_one_ending_balance,
+          account_one_starting_balance,
+          "1000000 wasn't the deploying account final balance"
+        );
+        assert.equal(
+          account_two_ending_balance,
+          account_two_starting_balance,
+          "0 wasn't the destination account final balance"
+        );
+      });
+  });
 });
