335466f25a69ec50a25158bf29673bddcced488b
Adam Gall <adam@decentcrypto.com>
2017-11-17 23:43:10 -0500

    Add Spectiv.sol smart contract to deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SignalTokenMock = artifacts.require("./SignalTokenMock.sol");
+var Spectiv = artifacts.require("./Spectiv.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SignalTokenMock);
+  deployer.deploy(Spectiv);
 };
