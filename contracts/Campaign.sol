7f9e4431d88e1ca3e7be8c7d4b371cd3eb72a435
Parker McCurley <parker@decentcrypto.com>
2017-11-20 11:33:41 -0500

    Scaffold 'Campaign' smart contract

---
+++ contracts/Campaign.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.4;
+
+
+contract Campaign {
+  address advertiser;
+  address publisher;
+  uint amount;
+
+  function Campaign(address _publisher, uint _amount) {
+    advertiser = msg.sender;
+    publisher = _publisher;
+    amount = _amount;
+  }
+
+  function execute() public {
+    initializeTransfer();
+  }
+
+  function initializeTransfer() private {
+    // Calls 'SignalTokenMock.transfer'
+  }
+}
