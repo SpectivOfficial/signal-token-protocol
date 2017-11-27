cf899c05acf29317395c0a0bd8299b7416dafa23
Parker McCurley <parker@decentcrypto.com>
2017-11-27 02:38:22 -0500

    create demo page

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-  if (typeof web3 !== "undefined") {
-    console.log("such web3 many testnets");
-  } else {
-    if (window.confirm("No web3 object is available. Press 'OK' to install Metamask.")) {
-      window.location.href = "https://metamask.io";
-    }
-  }
-});
