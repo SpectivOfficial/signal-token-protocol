d801d43dbb9e4946e4111594808daae4529fffd7
Parker McCurley <parker@decentcrypto.com>
2017-11-27 12:38:16 -0500

    Remove unneccessary local deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 const SignalTokenProtocol = artifacts.require("./SignalTokenProtocol.sol");
-const TokenStub = artifacts.require("./TokenStub.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SignalTokenProtocol);
-  deployer.deploy(TokenStub);
 };
